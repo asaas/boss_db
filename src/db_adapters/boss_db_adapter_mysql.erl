f924766f087c5f176857e191e776dd9bedde154c
michael <michael_nguyen@co-graph.com>
2014-02-14 16:08:59 +0900

    Merge changes in v0.8.9a from official repository.

--- src/db_adapters/boss_db_adapter_mysql.erl
+++ src/db_adapters/boss_db_adapter_mysql.erl
@@ -239,7 +239,7 @@ activate_record(Record, Metadata, Type) ->
                         undefined -> undefined;
                         {datetime, DateTime} -> boss_record_lib:convert_value_to_type(DateTime, AttrType);
                         Val -> 
-                            boss_sql_lib:convert_possible_foreign_key(RetypedForeignKeys, Type, Key, Val, AttrType, DBColumn)
+                            boss_sql_lib:convert_possible_foreign_key(RetypedForeignKeys, Type, Key, Val, AttrType)
                     end
             end, boss_record_lib:attribute_names(Type))).
 
@@ -418,7 +418,7 @@ pack_datetime(DateTime) ->
 pack_date(Date) ->
     "'" ++ erlydtl_filters:date(Date, "Y-m-d") ++ "'".
 
-pack_now(Now) -> pack_datetime(calendar:now_to_datetime(Now)).
+%pack_now(Now) -> pack_datetime(calendar:now_to_datetime(Now)).
 
 pack_value(null) ->
 	"null";
